408ac9aad0d64767a0aa4de35b5929679798dbc1
MarcosT16 <mtn.1029384756@gmail.com>
2024-06-22 12:03:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// =(atribuição)  ==(igual) !=(diferente) %(restante da divisão) /(divisão) *(multiplicação)  +=(acumulação)
+
+for(let i=1; i<100; i++){if(i%2 != 0){soma}}
+console.log(soma)
+
+
+
+
---
+++ script0.js
@@ -0,0 +1,11 @@
+let array = []
+array[0] = "Marcos"
+array[1] = "Thiago"
+array[2] = "Davi"
+array[3] = "Joseph"
+array[4] = "Joshua"
+
+for(let i=0; i<5; i++){
+    console.log(array[i])
+}
+    
